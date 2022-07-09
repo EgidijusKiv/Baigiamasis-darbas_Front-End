f44cec079736ece0e7497ef63108c99bc5419468
Egidijus Kivilis <egidijus.kivilis@gmail.com>
2022-07-10 00:48:10 +0300

    Pridetas Footer komponentas

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import Header from './components/Header'
+import Header from './components/Header';
 import Navigation from './components/Navigation';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <Navigation/>
-<Header edit={true}></Header>
+      <Navigation />
+      <Header edit={true} />
+      <Footer />
     </div>
   );
 }
