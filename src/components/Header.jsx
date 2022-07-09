703e338ef045fc2c81a5be88293c1b9e55d4f5a6
Egidijus Kivilis <egidijus.kivilis@gmail.com>
2022-07-10 00:52:51 +0300

    Aprasyti koregavimo mygtukai ir DELETE metodas

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,23 +2,49 @@ import './Header.css';
 import { useEffect, useState } from 'react';
 import { BiEdit } from 'react-icons/bi';
 import { RiDeleteBinLine } from 'react-icons/ri';
+import EditForm from './EditForm'
+import CreateUserForm from './CreateUserForm'
 
 export default function Header(props) {
   const [users, setUsers] = useState([]);
+  const [editUser, setEditUser] = useState(false);
+  const [userInfo, setUserInfo] = useState([]);
+  const [newUser, setNewUser] = useState(false);
+
   async function getUsers() {
     const result = await fetch('http://127.0.0.1:9000/users');
     const data = await result.json();
     setUsers(data);
   }
-  console.log(users);
+  async function deleteUser(id) {
+    await fetch(`http://127.0.0.1:9000/users/${id}`, {method: 'DELETE'} );
+    window.location.reload();
+  }
+
 
   useEffect(() => {
     getUsers();
   }, []);
 
-  return (
+  function showEditUser(user) {
+    setEditUser(!editUser)
+    setUserInfo(user)
+  }
+  function createNewUser() {
+    setNewUser(!newUser)  
+  }
 
+  return (
     <div className="header">
+      <button onClick={(e) => {
+        createNewUser();
+      }}>Test NewUser</button>
+      {editUser && <div>
+        <EditForm user={userInfo} />
+      </div>}
+      {newUser && <div>
+        <CreateUserForm />
+      </div>}
       <table className="header-table">
         <tbody>
           <tr>
@@ -27,29 +53,41 @@ export default function Header(props) {
             <th>Pavardė</th>
             <th>El.paštas</th>
             <th>Amžius</th>
-            <th></th>
+            <th />
           </tr>
           {users
             .map((user, i) => (
               <tr key={i}>
-                <td>{++i}</td>
+                <td>{`${i + 1}.`}</td>
                 <td>{user.first_name}</td>
                 <td>{user.last_name}</td>
                 <td>{user.email}</td>
                 <td>{user.age}</td>
                 {props.edit && (
-                <td>
-                  <button className="header-table edit" type="button">
-                    <span><BiEdit /></span>
-                    {' '}
-                    Koreguoti
-                  </button>
-                  <button className="header-table delete" type="button">
-                    <span><RiDeleteBinLine /></span>
-                    {' '}
-                    Ištrinti
-                  </button>
-                </td>
+                  <td>
+                    <button
+                      onClick={(e) => {
+                        e.preventDefault();
+                        showEditUser(user);
+                      }}
+                      className="header-table edit"
+                      type="button">
+                      <span><BiEdit /></span>
+                      {' '}
+                      Koreguoti
+                    </button>
+                    <button
+                      onClick={(e) => {
+                        e.preventDefault();
+                        deleteUser(user._id)
+                      }}
+                      className="header-table delete"
+                      type="button">
+                      <span><RiDeleteBinLine /></span>
+                      {' '}
+                      Ištrinti
+                    </button>
+                  </td>
                 )}
               </tr>
             ))}
