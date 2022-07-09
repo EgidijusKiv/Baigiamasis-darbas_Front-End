fd94ec1c87c4bd7988a60fa617d98ee31e262eac
Egidijus Kivilis <egidijus.kivilis@gmail.com>
2022-07-10 00:47:18 +0300

    Sukurtas Footer komponentas

---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import './Footer.css';
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <div className="footer">
+      <h5>
+        Users
+        {' '}
+        <span>App</span>
+        {' '}
+        2022 &copy; Made by
+        {' '}
+        <span>Egidijus Kivilis</span>
+      </h5>
+    </div>
+  );
+}
