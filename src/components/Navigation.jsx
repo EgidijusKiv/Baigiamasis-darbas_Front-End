2ef5f47392118ef502a513e0f227e09ea099db44
Egidijus Kivilis <egidijus.kivilis@gmail.com>
2022-07-10 00:49:00 +0300

    Pakoreguotas Navigation komponentas, pataisyti stiliai

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,23 +1,23 @@
-import "./Navigation.css"
+import './Navigation.css';
 import React from 'react';
 import logo from '../logo.svg';
 
+
 export default function Navigation() {
-    return (
-        <div className="navigation">
-            <div className='navigation-left'>
-                <img src={logo} alt="Logo" />
-                <p>React project</p>
+  return (
+    <div className="navigation">
+      <div className="navigation-left">
+        <img src={logo} alt="Logo" />
+        <p>React project</p>
 
-            </div>
-            <div className="navigation-middle">
-                <h3>Final Full-Stack Project</h3>
-            </div>
-            <div className='navigation-right'>
-                <button>Koreguoti vartotojus</button>
-            </div>
+      </div>
+      <div className="navigation-middle">
+        <h3>Final Full-Stack Project</h3>
+      </div>
+      <div className="navigation-right">
+        <button type="button">Koreguoti vartotojus</button>
+      </div>
 
-
-        </div>
-    )
+    </div>
+  );
 }
